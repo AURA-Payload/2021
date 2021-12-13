1598ae308353d297baf1ba3b88480217333e47f4
Fowler Askew <fowler.askew@gmail.com>
2021-12-12 22:32:23 -0600

    Create Example_toneGenerator.ino
    This file just screams out at 25kHz, created to test transmission signal strength

---
+++ marco/Example_toneGenerator/Example_toneGenerator.ino
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------
+// Example_toneGenerator.ino
+// Written by Fowler Askew
+// AURA Payload Team 2021
+// Plays a tone on a single pin for a defined amount of time
+// -----------------------------------------------------------
+
+#define TONE_PIN 9
+#define FREQ 25
+000
+#define ON_TIME 100
+#define OFF_TIME 250
+
+void setup() {
+  // blink to show that it's going
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13, LOW);
+  delay(250);
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13, LOW);
+  delay(250);
+
+  tone(TONE_PIN, FREQ);
+  digitalWrite(13, HIGH);
+  delay(ON_TIME);
+}
+
+void loop() {
+  // start playing the tone
+  // turn on the LED too
+//  tone(TONE_PIN, FREQ);
+//  digitalWrite(13, HIGH);
+//  delay(ON_TIME);
+//
+//  noTone(TONE_PIN);
+//  digitalWrite(13, LOW);
+//  delay(OFF_TIME);
+}
