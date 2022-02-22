2f09791ebdea46155283859ff3885d6b4cac204b
Fowler Askew <fowler.askew@gmail.com>
2022-02-22 16:50:42 -0600

    Adapted for MARCO test board
    Modified tone pin number and frequency for testing tone generation on the MARCO board

--- marco/Example_toneGenerator/Example_toneGenerator.ino
+++ marco/Example_toneGenerator/Example_toneGenerator.ino
@@ -5,11 +5,10 @@
 // Plays a tone on a single pin for a defined amount of time
 // -----------------------------------------------------------
 
-#define TONE_PIN 9
-#define FREQ 25
-000
-#define ON_TIME 100
-#define OFF_TIME 250
+#define TONE_PIN 38
+#define FREQ 1000
+#define ON_TIME 1000
+#define OFF_TIME 1000
 
 void setup() {
   // blink to show that it's going
@@ -22,19 +21,19 @@ void setup() {
   digitalWrite(13, LOW);
   delay(250);
 
-  tone(TONE_PIN, FREQ);
-  digitalWrite(13, HIGH);
-  delay(ON_TIME);
+//  tone(TONE_PIN, FREQ);
+//  digitalWrite(13, HIGH);
+//  delay(ON_TIME);
 }
 
 void loop() {
   // start playing the tone
   // turn on the LED too
-//  tone(TONE_PIN, FREQ);
-//  digitalWrite(13, HIGH);
-//  delay(ON_TIME);
-//
-//  noTone(TONE_PIN);
-//  digitalWrite(13, LOW);
-//  delay(OFF_TIME);
+  tone(TONE_PIN, FREQ);
+  digitalWrite(13, HIGH);
+  delay(ON_TIME);
+
+  noTone(TONE_PIN);
+  digitalWrite(13, LOW);
+  delay(OFF_TIME);
 }
