e11009481f4a997f92afb4203f1becf1e55ae6b8
Fowler Askew <fowler.askew@gmail.com>
2022-02-25 21:18:36 -0600

    Create EASE_alive.ino
    This is a file I'm using to make sure the EASE microcontroller will actually work

---
+++ EASE/EASE_alive/EASE_alive.ino
@@ -0,0 +1,25 @@
+/*
+  EASE alive
+  I'm using this sketch to test if the microcontroller is working at all before trying more complex functions
+ */
+
+ 
+int led = 18;
+int led2 = 19;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led2, LOW);
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led2, HIGH);
+  delay(1000);               // wait for a second
+}
