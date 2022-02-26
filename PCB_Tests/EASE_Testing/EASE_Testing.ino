1b9076f8878d4f4b5e052de1192343eaa2e10969
Fowler Askew <fowler.askew@gmail.com>
2022-02-25 22:04:56 -0600

    Update EASE_Testing.ino
    Added BMP280 and ADXL345 init code, removed LSM6 and LPS init code, removed extra radio code

--- PCB_Tests/EASE_Testing/EASE_Testing.ino
+++ PCB_Tests/EASE_Testing/EASE_Testing.ino
@@ -11,11 +11,10 @@
 #define LIMIT_2 17
 
 #include <RadioLib.h>  // include radio library
-#include <SdFat.h>  // include SD library
 #include <Wire.h>
-#include <LSM6.h>
-#include <LPS.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include <Adafruit_BMP280.h>
 
 // motor pins
 #define PWM_A 6 // PWM pin, motor A
@@ -31,13 +30,10 @@
 
 RFM95 radio = new Module(CSPIN, DIO0PIN, NRSTPIN, DIO1PIN);  // radio object
 
-LSM6 imu;  // sensor stuff
-LPS ps;
-//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
-//Adafruit_BMP280 bmp; // I2C
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+Adafruit_BMP280 bmp; // I2C
 
 float BMPcal = 0;
-float LPScal = 0;
 
 // transmit variables
 unsigned int transmitTimer = 0;  // stores the time of the last transmission
@@ -63,6 +59,9 @@ void setup()
 
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
+
+  pinMode(LIMIT_1, INPUT_PULLUP);
+  pinMode(LIMIT_2, INPUT_PULLUP);
 
   Serial.begin(115200);
   Wire.begin();
@@ -91,29 +90,34 @@ void setup()
   }
 
   radio.setDio0Action(setFlag);  // function that will be called when something is done
-
-  Serial.println("Listening for packets");
-  receiveState = radio.startReceive();  // start listening
   // ----- END RADIO SETUP -----
 
-  // ----- LSM6DS33 IMU SETUP -----
-  if (!imu.init())
-    Serial.println("LSM6DS33 initialization failed");
+  // ----- ADXL345 ACCELEROMETER SETUP -----
+  if(!accel.begin())
+    Serial.println("ADXL345 initialization failed");
   else
-    Serial.println("LSM6DS33 initialized");
-
-  imu.enableDefault();
-  // ODR_XL = 1000 (1.66 kHz (high performance))
-  // FS_XL = 01 (+/-16 g full scale)
-  // BW_XL = 00 (400 Hz) (Overridden when XL_BW_SCAL_ODR=0)
-  imu.writeReg(LSM6::CTRL1_XL, 0b10000100);
-  // ODR = 1000 (1.66 kHz (high performance))
-  // FS_G = 11 (2000 dps)
-  imu.writeReg(LSM6::CTRL2_G, 0b10001100);
-
+    Serial.println("ADXL345 initialized");
+  
+  accel.setRange(ADXL345_RANGE_16_G);
+  
   delay(500);
-  LPScal = ps.readPressureMillibars();
-  LPScal = ps.pressureToAltitudeMeters(LPScal);
+  
+  // ----- BMP280 ALTIMETER SETUP -----
+  if (!bmp.begin())
+    Serial.println("BMP280 initialization failed");
+  else
+    Serial.println("BMP280 initialized");
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_OFF,
+                  Adafruit_BMP280::STANDBY_MS_500);
+  
+  delay(500);
+  BMPcal = bmp.readAltitude(LOCALPRESSURE);
+
+  
 
   Serial.println("Startup complete");
 
@@ -129,6 +133,8 @@ void loop()
   delay(2000);
   analogWrite(PWM_A, 0);
 
+  delay(250);
+
   digitalWrite(DIR_A, LOW);
   analogWrite(PWM_A, 100);
   delay(2000);
@@ -140,6 +146,8 @@ void loop()
   delay(2000);
   analogWrite(PWM_B, 0);
 
+  delay(250);
+  
   digitalWrite(DIR_B, LOW);
   analogWrite(PWM_B, 100);
   delay(2000);
@@ -165,92 +173,6 @@ void loop()
   }
   
   digitalWrite(LED_1, LOW);
-}
-
-void setFlag(void)  // this function is called after complete packet transmission or reception
-{
-  if(!enableInterrupt)  // check if the interrupt is enabled
-    return;
-
-  operationDone = true;  // something happened, set the flag
-}
-
-void handleReceive()  // performs everything necessary when data comes in
-{
-  receiveState = radio.readData(RXarray, 7);  // read received data to array
-
-  if (receiveState == RADIOLIB_ERR_NONE)  // packet received correctly
-  {
-//    Serial.println(F("[RFM95] Received packet!"));
-//
-//    Serial.print(F("[RFM95] Data:\t\t"));  // print data
-//    Serial.print(RXarray[0]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[1]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[2]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[3]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[4]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[5]);
-//    Serial.print("\t");
-//    Serial.print(RXarray[6]);
-    
-    lastRSSI = radio.getRSSI();  // get RSSI to be reported
-    
-//    Serial.print(F("\t[RFM95] RSSI: "));  // print RSSI if desired
-//    Serial.print(lastRSSI);
-//    Serial.println(F(" dBm"));
-   
-    if(RXarray[0])
-      controls[0] = -1 * RXarray[1];
-    else
-      controls[0] = RXarray[1];
-    
-    if(RXarray[2])
-      controls[1] = -1 * RXarray[3];
-    else
-      controls[1] = RXarray[3];
-    
-    if(RXarray[4])
-      controls[2] = -1 * RXarray[5];
-    else
-      controls[2] = RXarray[5];
-
-    controls[3] = RXarray[6];
-  }
-//  else if (receiveState == ERR_CRC_MISMATCH)  // packet received malformed
-//    Serial.println(F("[RFM95] CRC error!"));
-//  else  // some other error
-//  {
-//    Serial.print(F("[RFM95] Failed, code "));
-//    Serial.println(receiveState);
-//  }
-}
-
-void transmitData()
-{
-  String telemetryString = "";
-  
-  telemetryString += String(controls[0]);
-  telemetryString += ", ";
-  telemetryString += String(controls[1]);
-  telemetryString += ", ";
-  telemetryString += String(controls[2]);
-  telemetryString += ", ";
-  telemetryString += String(controls[3]);
-  telemetryString += ", ";
-  telemetryString += String(lastRSSI);
-  telemetryString += "dBm\n";
-  
-  transmitFlag = true;
-  txComplete = false;
-  transmitTimer = millis();  // reset transmit timer
-  
-  //Serial.println(F("[RFM95] Sending string ... "));
-  transmitState = radio.startTransmit(telemetryString);  // transmit array 1
 }
 
 void setMotors()
