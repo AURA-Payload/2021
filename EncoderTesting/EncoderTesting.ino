791d189d5da6d0874ee2c690d3fc05a5daefd825
dss0029 <dss0029@auburn.edy>
2022-03-23 19:54:31 -0500

    Update EncoderTesting.ino

--- EncoderTesting/EncoderTesting.ino
+++ EncoderTesting/EncoderTesting.ino
@@ -1,23 +1,16 @@
-#define LOCALPRESSURE 1016.8  // used to calculate altitude
-
-#include <RadioLib.h>  // include radio library
-#include <SdFat.h>  // include SD library
-#include <LSM6.h>
-#include <LPS.h>
-
 #define PWM_1 7  // PWM pin, motor 1
 #define DIR_1 8 // Direction pin, motor 1
 #define interruptPin 6 //Interrupt pin
 
-int screwLength = 17; //inches
-int gearRatio = 3500; //gearRatio * rotationsOfScrew = rotationsOfMotor
+int screwLength = .125; //inches
+int gearRatio = 340; //gearRatio * rotationsOfScrew = rotationsOfMotor
 int rotations = 0;
 int extended = 0; //0 for false, 1 for true. 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE); //If value of interruptPin changes, run count. 
+  attachInterrupt(digitalPinToInterrupt(interruptPin), count, RISING); //If value of interruptPin changes, run count. 
   digitalWrite(DIR_1, HIGH); //Set motor direction forward
   digitalWrite(PWM_1, HIGH); //Turn on motor
 }
@@ -33,7 +26,7 @@ void loop() {
 
 void count() {
   rotations++;
-  if(rotations/(gearRatio * 8) >= 17){ //Check to see if fully extended. If so, set extended. 
+  if(rotations/(gearRatio * 8) >= screwLength){ //Check to see if fully extended. If so, set extended. 
     extended = 1;
   }
 }
