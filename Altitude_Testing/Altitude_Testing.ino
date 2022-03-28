ecbd44e02a5e56ca62b25721bf768d4e40bf7edb
dss0029 <dss0029@auburn.edy>
2022-03-28 18:47:28 -0500

    Update Altitude_Testing.ino

--- Altitude_Testing/Altitude_Testing.ino
+++ Altitude_Testing/Altitude_Testing.ino
@@ -1,11 +1,12 @@
 #define LOCALPRESSURE 1016.8  // used to calculate altitude
 
 #include <Wire.h>
-#include <LSM6.h>
-#include <LPS.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include <Adafruit_BMP280.h>
 
-LSM6 imu;  // sensor stuff
-LPS ps;
+Adafruit_BMP280 bmp; // I2C
+
 float initAlt = 0;
 float currentAlt = 10000;
 float checkAlt1 = 10000;
@@ -15,25 +16,21 @@ int altRange = 1; //Amount of meters the rocket can be above the starting value.
 
 void setup() {
   // put your setup code here, to run once:
-  // ----- LSM6DS33 IMU SETUP -----
-  if (!imu.init())
-    Serial.println("LSM6DS33 initialization failed");
+  // ----- BMP280 ALTIMETER SETUP -----
+  if (!bmp.begin())
+    Serial.println("BMP280 initialization failed");
   else
-    Serial.println("LSM6DS33 initialized");
+    Serial.println("BMP280 initialized");
 
-  imu.enableDefault();
-  // ODR_XL = 1000 (1.66 kHz (high performance))
-  // FS_XL = 01 (+/-16 g full scale)
-  // BW_XL = 00 (400 Hz) (Overridden when XL_BW_SCAL_ODR=0)
-  imu.writeReg(LSM6::CTRL1_XL, 0b10000100);
-  // ODR = 1000 (1.66 kHz (high performance))
-  // FS_G = 11 (2000 dps)
-  imu.writeReg(LSM6::CTRL2_G, 0b10001100);
-
-  delay(500);
-  initAlt = ps.readPressureMillibars();
-  initAlt = ps.pressureToAltitudeMeters(initAlt);
-
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_OFF,
+                  Adafruit_BMP280::STANDBY_MS_500);
+  
+  delay(20000);
+  initAlt = bmp.readAltitude(LOCALPRESSURE);
+  
   Serial.println("Startup complete");
 }
 
@@ -41,15 +38,12 @@ void loop() {
   // Check current altitude. If current altitude is close enough to intial value, then enter landing detection.
   // Get altitude, wait 2 seconds, get altitude again, and compare. If the 2 values are close, we have probably landed. 
   while(landed == 0){
-    currentAlt = ps.readPressureMillibars();
-    currentAlt = ps.pressureToAltitudeMeters(currentAlt);
+    currentAlt = bmp.readAltitude(LOCALPRESSURE);
     if(currentAlt <= initAlt + altRange){
-      checkAlt1 = ps.readPressureMillibars();
-      checkAlt1 = ps.pressureToAltitudeMeters(checkAlt1); 
+      checkAlt1 = bmp.readAltitude(LOCALPRESSURE);
       delay(2000);
-      checkAlt2 = ps.readPressureMillibars();
-      checkAlt2 = ps.pressureToAltitudeMeters(checkAlt2); 
-      if(checkAlt1 - altRange < checkAlt2 < checkAlt1 + altRange){
+      checkAlt2 = bmp.readAltitude(LOCALPRESSURE);
+      if(checkAlt1 - 1 < checkAlt2 < checkAlt1 + 1){
         landed = 1;
         Serial.println("Landed");
       }
