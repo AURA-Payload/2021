291f869ad947acd7f3d9ff96000ac781381b2fcf
Fowler Askew <fowler.askew@gmail.com>
2022-03-30 20:53:49 -0500

    Update EncoderTesting.ino
    Changed to the pins used by the POLO board so that it can be used for testing.
    Also updated the interrupt to trigger on state change instead of rising edge

--- EncoderTesting/EncoderTesting.ino
+++ EncoderTesting/EncoderTesting.ino
@@ -1,17 +1,17 @@
-#define PWM_1 7  // PWM pin, motor 1
-#define DIR_1 8 // Direction pin, motor 1
-#define interruptPin 6 //Interrupt pin
+#define PWM_1 5  // PWM pin, motor 1
+#define DIR_1 4 // Direction pin, motor 1
+#define interruptPin 2 //Interrupt pin
 
-int screwLength = .125; //inches
+int travelDistance = .125; //inches
 int gearRatio = 340; //gearRatio * rotationsOfScrew = rotationsOfMotor
-int pulsePerRotate = 12;
+int pulsePerRotate = 22;
 int pulseCount = 0;
 int extended = 0; //0 for false, 1 for true. 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), count, RISING); //If value of interruptPin changes, run count. 
+  attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE); //If value of interruptPin changes, run count. 
   digitalWrite(DIR_1, HIGH); //Set motor direction forward
   digitalWrite(PWM_1, HIGH); //Turn on motor
 }
@@ -19,7 +19,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // If screw has not been fully extended, keep going. Otherwise, stop. 
-  if(extended == 1){
+  if(extended == 1)
+  {
     digitalWrite(PWM_1, LOW); //Turn off motor
   }
  
@@ -27,7 +28,7 @@ void loop() {
 
 void count() {
   pulseCount++;
-  if(pulseCount/(gearRatio * pulsePerRotate * 8) >= screwLength){ //Check to see if fully extended. If so, set extended. 
+  if(pulseCount/(gearRatio * pulsePerRotate * 8) >= travelDistance){ //Check to see if fully extended. If so, set extended. 
     extended = 1;
   }
 }
