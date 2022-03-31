3a9529fc1414223fc3c714bac00a355a96abc736
Fowler Askew <fowler.askew@gmail.com>
2022-03-30 21:55:17 -0500

    Update EncoderTesting.ino
    This is the result of my testing today. The encoder is sometimes accurate, but it isn't very repeatable. I think the inputs may be bouncing, so code may be needed to fix that

--- EncoderTesting/EncoderTesting.ino
+++ EncoderTesting/EncoderTesting.ino
@@ -1,34 +1,37 @@
-#define PWM_1 5  // PWM pin, motor 1
-#define DIR_1 4 // Direction pin, motor 1
-#define interruptPin 2 //Interrupt pin
+#define PWM_1 5  // PWM pin, motor 1 (7 for EASE board)
+#define DIR_1 4 // Direction pin, motor 1 (8 for EASE board)
+#define interruptPin 2 //Interrupt pin (6 for EASE board)
 
-int travelDistance = .125; //inches
-int gearRatio = 340; //gearRatio * rotationsOfScrew = rotationsOfMotor
-int pulsePerRotate = 22;
-int pulseCount = 0;
-int extended = 0; //0 for false, 1 for true. 
+int totalDistance = 1; //number of motor shaft rotations to complete
+int gearRatio = 340; // gearRatio * rotationsOfScrew = rotationsOfMotor
+int pulsePerRotate = 23;  // encoder pulses (rising and falling) for one rotation
+int screwPitch = 1;  // TPI of leadscrew
+volatile int pulseCount = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(DIR_1, OUTPUT);
+  pinMode(PWM_1, OUTPUT);
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE); //If value of interruptPin changes, run count. 
+  delay(10);
+  Serial.println("Turning on motor");
+  delay(10);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), count, FALLING); //If value of interruptPin changes, run count. 
   digitalWrite(DIR_1, HIGH); //Set motor direction forward
-  digitalWrite(PWM_1, HIGH); //Turn on motor
+  analogWrite(PWM_1, 0); //Turn on motor, low speed
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // If screw has not been fully extended, keep going. Otherwise, stop. 
-  if(extended == 1)
-  {
-    digitalWrite(PWM_1, LOW); //Turn off motor
-  }
- 
+  Serial.println(pulseCount);
 }
 
-void count() {
+void count()
+{
   pulseCount++;
-  if(pulseCount/(gearRatio * pulsePerRotate * 8) >= travelDistance){ //Check to see if fully extended. If so, set extended. 
-    extended = 1;
-  }
+  
+  /*if(pulseCount >= (totalDistance * gearRatio * pulsePerRotate * screwPitch)) //Check to see if fully extended. If so, change value or something. 
+  {
+    detachInterrupt(digitalPinToInterrupt(interruptPin)); //stop interrupting
+    analogWrite(PWM_1, 0); //Turn off motor
+  }*/
 }
