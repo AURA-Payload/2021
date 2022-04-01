d247b7993b0177f273d3d07aef057e7c00c00981
Fowler Askew <fowler.askew@gmail.com>
2022-03-31 23:28:53 -0500

    Updated sundial code - latency fix
    The analog signal from the light sensor takes about 150 microseconds to propagate through the multiplexer. I implemented a delay to read the values accurately

--- misc/SundialTesting/SundialTesting.ino
+++ misc/SundialTesting/SundialTesting.ino
@@ -20,16 +20,17 @@ void setup() {
   pinMode(BIT2, OUTPUT);
   pinMode(BIT3, OUTPUT);
 
-  pinMode(TIME1, INPUT);
-  pinMode(TIME2, INPUT);
+  pinMode(SENSE1, INPUT);
+  pinMode(SENSE2, INPUT);
 
   updateSelection(0);  // writes 0000 to sensor select pins
 }
 
-void loop() {
+void loop()
+{
   readSensors();
 
-  printSensors();
+  printArray(sensorValues, 0, 32);
 }
 
 void readSensors()  // reads all the sensors into the sensorValues array
@@ -37,28 +38,29 @@ void readSensors()  // reads all the sensors into the sensorValues array
   for(byte readLoop = 0; readLoop < 16; readLoop++)
   {
     updateSelection(readLoop);
+    delayMicroseconds(200);  // this delay allows the analog signal to propage before being read (see POLO trello card for 'scope measurement)
     sensorValues[readLoop] = analogRead(SENSE1);  // read the values into an array
     sensorValues[readLoop + 16] = analogRead(SENSE2);
   }
 }
 
-void printSensors()  // print the array of collected sensor data
+void printArray(int arrayIn[], int printStart, int printEnd)  // print an array
 {
-  for(byte printLoop = 0; printLoop < 32; printLoop++)
+  for(byte printLoop = printStart; printLoop < printEnd; printLoop++)
   {
-    Serial.print(sensorValues[printLoop]);
-    Serial.print(",");
+    Serial.print(arrayIn[printLoop]);
+    Serial.print("\t");
   }
   Serial.print('\n');
 }
 
 void updateSelection(byte selectVar) {
   bool bitVal = selectVar & 0b00000001; // set LSB
-  digitalWrite(BIT0, val);
+  digitalWrite(BIT0, bitVal);
   bitVal = selectVar & 0b00000010;  // set bit 1
-  digitalWrite(BIT1, val);
+  digitalWrite(BIT1, bitVal);
   bitVal = selectVar & 0b00000100;  // set bit 2
-  digitalWrite(BIT2, val);
+  digitalWrite(BIT2, bitVal);
   bitVal = selectVar & 0b00001000;  // set MSB
-  digitalWrite(BIT3, val);
+  digitalWrite(BIT3, bitVal);
 }
