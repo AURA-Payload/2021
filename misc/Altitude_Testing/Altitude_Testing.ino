60c161275aa012a407adea1a004aee6cbce07aa1
Fowler Askew <fowler.askew@gmail.com>
2022-04-01 22:30:25 -0500

    Update Altitude_Testing.ino
    Added some checks to make sure the rocket has settled on the ground

--- misc/Altitude_Testing/Altitude_Testing.ino
+++ misc/Altitude_Testing/Altitude_Testing.ino
@@ -2,17 +2,19 @@
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
 #include <Adafruit_BMP280.h>
 
 Adafruit_BMP280 bmp; // I2C
 
-float initAlt = 0;
+float initAlt = 0;  // stores altitude at ground level
 float currentAlt = 10000;
 float checkAlt1 = 10000;
 float checkAlt2 = 10000;
-int landed = 0;
-int altRange = 1; //Amount of meters the rocket can be above the starting value. 
+bool launched = 0;
+bool landed = 0;
+int launchThresh = 2;  // rocket must pass this altitude for landing detection to proceed
+int altRange = 1; //Amount of meters (+ or -) the rocket can be above/below the starting value.
+float noiseLimit = 0.1;  // amount of noise allowed between values after landing - lower will make it trigger when the rocket is more still
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,7 +31,11 @@ void setup() {
                   Adafruit_BMP280::STANDBY_MS_500);
   
   delay(20000);
-  initAlt = bmp.readAltitude(LOCALPRESSURE);
+  
+  initAlt = bmp.readAltitude(LOCALPRESSURE);  // calibrate ground level altitude
+
+  Serial.print("Altitude initialized at: ");
+  Serial.println(initAlt);
   
   Serial.println("Startup complete");
 }
@@ -37,16 +43,28 @@ void setup() {
 void loop() {
   // Check current altitude. If current altitude is close enough to intial value, then enter landing detection.
   // Get altitude, wait 2 seconds, get altitude again, and compare. If the 2 values are close, we have probably landed. 
-  while(landed == 0){
-    currentAlt = bmp.readAltitude(LOCALPRESSURE);
-    if(currentAlt <= initAlt + altRange){
-      checkAlt1 = bmp.readAltitude(LOCALPRESSURE);
-      delay(2000);
-      checkAlt2 = bmp.readAltitude(LOCALPRESSURE);
-      if(checkAlt1 - 1 < checkAlt2 < checkAlt1 + 1){
+  currentAlt = bmp.readAltitude(LOCALPRESSURE) - initAlt;  // make the altitude reading relative to ground level
+  if(!launched && currentAlt > launchThresh)
+  {
+    launched = 1;
+    Serial.println("Launched");
+  }
+    
+  if(launched && !landed)
+  {
+    if(currentAlt <= altRange && currentAlt >= -altRange)
+    {
+      checkAlt1 = bmp.readAltitude(LOCALPRESSURE) - initAlt;
+      delay(250);
+      checkAlt2 = bmp.readAltitude(LOCALPRESSURE) - initAlt;
+      if(checkAlt1 - noiseLimit < checkAlt2 < checkAlt1 + noiseLimit)
+      {
         landed = 1;
         Serial.println("Landed");
       }
     }
   }
+  
+  Serial.println(currentAlt);
+  delay(500);
 }
