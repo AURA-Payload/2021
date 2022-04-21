e6dc2eee6dbef9310a8c4098e860287b2b1d8bb0
Fowler Askew <fowler.askew@gmail.com>
2022-04-21 18:57:30 -0500

    Update readUltrasound.ino
    Added minimum sampling, increased dynamic range, added low-pass filtering

--- misc/readUltrasound/readUltrasound.ino
+++ misc/readUltrasound/readUltrasound.ino
@@ -5,18 +5,22 @@
 // Continually reads the incoming signal from the ultrasonic amplifier
 // -----------------------------------------------------------
 
-#define TONE_IN   // input pin is A2 (DIO16)
-#define sampleDuration 25  // slightly longer than half the input signal period (40us/2 + 5)
-int sampleStart = 0;
+#define TONE_IN A2  // input pin is A2 (DIO16)
+#define sampleDuration 30  // slightly longer than half the input signal period (40us/2 + 10)
+#define ultrasoundThreshold 300  // 
+unsigned int sampleStart = 0;
 int sampleEnd = 0;
 int maxInput = 0;
+int minInput = 1024;
 int currentInput = 0;
+int strength = 500;
+double smoothingVal = 0.05;
 
 void setup()
 {
   pinMode(TONE_IN, INPUT_DISABLE);  // I think this is beneficial on Teensy boards
   //pinMode(TONE_IN, INPUT);  // not strictly necessary, but still good
-  //analogReadAveraging(4);  // number of samples to average (default 4)
+  analogReadAveraging(1);  // number of samples to average (default 4)
   Serial.begin(115200);
   delay(500);
 }
@@ -24,6 +28,7 @@ void setup()
 void loop()
 {
   maxInput = analogRead(TONE_IN);  // this will be updated to contain the max sample value
+  minInput = analogRead(TONE_IN);
   sampleStart = micros();
   while(micros() < sampleStart + sampleDuration)
   {
@@ -31,8 +36,11 @@ void loop()
     
     if(currentInput > maxInput)  // if current sample is greater than previous max sample
       maxInput = currentInput;  // update max sample
+      
+    if(currentInput < minInput)  // update min sample
+      minInput = currentInput;
   }
-
-  Serial.println(maxInput);
-  delay(100);  // delay 100 milliseconds to decrease the printing
+  strength = smoothingVal*(maxInput - minInput) + (1-smoothingVal)*strength;
+  Serial.println(strength);
+  delay(5);  // delay 100 milliseconds to decrease the printing
 }
